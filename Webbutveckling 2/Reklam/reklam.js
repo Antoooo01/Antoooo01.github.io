01e0027cdd29a051decf633d8105466abc4241d5
Antoooo01 <83810618+Antoooo01@users.noreply.github.com>
2021-09-01 11:27:09 +0200

    init

---
+++ Webbutveckling 2/Reklam/reklam.js
@@ -0,0 +1,8 @@
+var x = document.getElementById("body");
+x.style.backgroundColor = "rgb(255, 255, 255)";
+
+// for (let index = 0; index < 255; index++) {
+//     x.style.backgroundColor = `rgb(${index}, ${index}, ${index})`;
+    
+// }
+
